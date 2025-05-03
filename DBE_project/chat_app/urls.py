969faec05d0d3ff3bf99ba5f42ae73a12fa070e2
Davy Nziengui <nzienguirock2004@gmail.com>
2025-05-03 17:51:52 +0200

    Add files via upload

---
+++ DBE_project/chat_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.chatroom_list, name='chatroom_list'),
+    path('create/', views.create_channel_view, name='create_chatroom'),
+    path('<int:room_id>/', views.chatroom_detail, name='chatroom_detail'),
+    path('channel/<int:room_id>/delete/', views.delete_channel, name='delete_channel'),
+    path('channel/<int:room_id>/join/', views.join_channel, name='join_channel'),
+    path('channel/<int:room_id>/leave/', views.leave_channel, name='leave_channel'),
+    path('my-channels/', views.user_channels, name='user_channels'),
+]
---
+++ README.md
@@ -0,0 +1,7 @@
+# TW_PROJET
+ 
+Project Techno-Web2 
+
+Bastien DIDIERJEAN
+Elise BEAUVY
+Roch Davy Huffel NZIENGUI MBADINGA
